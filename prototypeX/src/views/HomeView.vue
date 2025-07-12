a05d251d64072aa5671a0af480cd305f51a0b28b
Zhou Linquan <168798258+Zha0fusion@users.noreply.github.com>
2025-07-12 10:17:21 +0800

    初始化配置

--- prototypeX/src/views/HomeView.vue
+++ prototypeX/src/views/HomeView.vue
@@ -1,9 +1,10 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+        <h1>Test Page</h1>
+    <p>泥嚎(*´▽｀)ノノ这里是隐山的主页</p>
+    <p>或许哪天有空他会想起来装修这里(被打</p>
   </main>
 </template>
--- prototypeX/vite.config.ts
+++ prototypeX/vite.config.ts
@@ -13,6 +13,36 @@ export default defineConfig({
       registerType: 'autoUpdate',
       devOptions: {
         enabled: true
+      },
+      manifest: {
+        name: 'PrototypeX',
+        short_name: 'PtypeX',
+        start_url: '/',
+        display: 'standalone',
+        theme_color: '#000000',
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          },
+          {
+            src: 'maskable-icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'maskable'
+          },
+          {
+            src: 'apple-touch-icon-180x180.png',
+            sizes: '512x512',
+            type: 'image/png',
+          }
+        ]
       }
     }),
   ],
